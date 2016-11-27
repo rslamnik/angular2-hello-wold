7ad18bd4b5030d239cc235172213b915d947ced4
Reuf Slamnik <reuf.slamnik@razberi.net>
2016-11-27 22:05:25 +0100

    Commited solution

---
+++ app/brave.component.ts
@@ -0,0 +1,18 @@
+﻿import { Component } from '@angular/core'
+
+@Component({
+
+    selector: 'brave-component',
+    template: 'Hi, I am {{name}}'
+
+})
+
+export class BraveComponent {
+
+    name: String;
+
+    constructor() {
+        this.name = 'The Most Brave Component';
+    }
+
+}
---
+++ app/brave.module.ts
@@ -0,0 +1,13 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BraveComponent } from './brave.component';
+
+@NgModule({
+
+    imports: [BrowserModule],
+    declarations: [BraveComponent],
+    bootstrap: [BraveComponent]
+
+})
+
+export class BraveModule { }
